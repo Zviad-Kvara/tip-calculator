e44958de5e00b66c4765d1a2cc9b373d45f262d9
Zviad Kvaratskhelia <kvara007@gmail.com>
2023-07-13 18:10:56 +0400

    update JavaScript

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 const billInput = document.getElementById("bill-input");
-// ----------tip-btns
+// --------tip-btns--------
 const tips = document.getElementsByClassName("tip-btns");
 const tipsArray = Array.from(tips);
-// ------------------
+// ------------------------
 const peopleInput = document.getElementById("people");
+const tipAmount = document.getElementById("tip-amount-txt");
 const totalResult = document.getElementById("total-amount");
+const resetBtn = document.getElementById("reset-btn");
+const validation = document.getElementById("validation");
 
 let billValue = 0;
 let tipsValue = 0;
 let peopleValue = 0;
 let total = 0;
+let tipResult = 0;
 billInput.addEventListener("input", event => {
   billValue = parseInt(event.target.value);
   calculation();
@@ -24,18 +28,36 @@ for (let i = 0; i < tipsArray.length; i++) {
 
 peopleInput.addEventListener("input", event => {
   peopleValue = parseInt(event.target.value);
+  if (peopleValue === 0) {
+    validation.innerText = "Cant be Zero";
+    peopleInput.style.border= "solid #E17052";
+  } else {
+    validation.innerText = "";
+    peopleInput.style.border="none"
+  }
   calculation();
 });
 
-// calculation
 function calculation() {
-  total = (billValue + (billValue * (tipsValue / 100)) / peopleValue).toFixed(
+  total = ((billValue + billValue * (tipsValue / 100)) / peopleValue).toFixed(
     2
   );
 
-  if ((peopleValue === 0) | (peopleValue === peopleValue.length)) {
+  if (peopleValue === 0 || peopleValue.length === 0 || isNaN(total) || isNaN(tipResult)|| tipResult === 0) {
     totalResult.innerText = "0.00$";
+    tipAmount.innerText = "0.00$";
   } else {
     totalResult.innerText = `${total}$`;
+    tipAmount.innerText = `${tipResult}$`;
   }
+
+  tipResult = ((billValue * tipsValue) / 100 / peopleValue).toFixed(2);
 }
+
+resetBtn.addEventListener("click", () => {
+  billInput.value = "";
+  peopleInput.value = "";
+  tipsValue = 0;
+  totalResult.innerText = "0.00$";
+  tipAmount.innerText = "0.00$";
+});
